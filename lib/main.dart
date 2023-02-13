55b75697a1fb426670d60316b5cf53d66c94ae01
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2023-02-13 15:47:13 -0600

    grid example

--- lib/main.dart
+++ lib/main.dart
@@ -39,23 +39,91 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    right: 5,
+                  ),
+                  height: 80,
+                  width: 80,
+                  color: Colors.orange,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 5,
+                    right: 10,
+                  ),
+                  height: 80,
+                  width: 80,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  height: 80,
+                  color: Colors.pink,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  left: 10,
+                  right: 5,
+                ),
+                height: 80,
+                width: (size.width - 40) / 3,
+                color: Colors.teal,
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 5,
+                  right: 5,
+                ),
+                height: 80,
+                width: (size.width - 40) / 3,
+                color: Colors.blue,
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 5,
+                  right: 10,
+                ),
+                height: 80,
+                width: (size.width - 40) / 3,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
